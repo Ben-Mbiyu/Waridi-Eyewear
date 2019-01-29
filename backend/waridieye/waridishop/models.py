48e8daaa4e93a4953a2bc6f4f7af5248f005333f
Ben-Mbiyu <benedictmbiyuthangwa@gmail.com>
2019-01-29 12:25:15 +0300

    Create database models and relationships

--- backend/waridieye/waridishop/models.py
+++ backend/waridieye/waridishop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    phone = models.PositiveIntegerField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='media/')
+    specs = models.TextField()
+    price = models.PositiveIntegerField()
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
