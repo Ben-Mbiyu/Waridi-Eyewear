7e741aaf0485801f332ceccd85c9ba604d2577a0
Ben-Mbiyu <benedictmbiyuthangwa@gmail.com>
2019-01-29 12:41:46 +0300

    Activate django admin and register models on admin.py

--- backend/waridieye/waridishop/models.py
+++ backend/waridieye/waridishop/models.py
@@ -5,12 +5,21 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     phone = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=150)
     image = models.ImageField(upload_to='media/')
     specs = models.TextField()
     price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
